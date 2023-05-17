fba007adf3de410ba80186fbc223fe529fbf9eb5
Jakob Giese <jakobgoodnessgracious@gmail.com>
2023-05-17 15:19:07 -0400

    rm unused

--- bundle.js
+++
@@ -1,8 +0,0 @@
-(function (TReact, main) {
-	'use strict';
-
-	var domNode = document.getElementById('root');
-	var root = TReact.createRoot(domNode);
-	root.render( /*#__PURE__*/React.createElement(main.UsingFragment, null));
-
-})(TReact, main);
